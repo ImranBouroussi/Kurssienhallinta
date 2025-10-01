fea76a74c12d2e287d980482da8eb52a3a88d980
ImranBouroussi <155960049+ImranBouroussi@users.noreply.github.com>
2025-10-01 12:54:38 +0300

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+</main>
+<footer class="text-center small text-muted mb-4">KURSSIHALLINTAJÄRJESTELMÄ</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
