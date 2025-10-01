32d93740728ce26ce570cfab3db0f188dc454c2b
ImranBouroussi <155960049+ImranBouroussi@users.noreply.github.com>
2025-10-01 13:20:42 +0300

    Create delete.php

---
+++ teachers/delete.php
@@ -0,0 +1,8 @@
+<?php require_once __DIR__ . '/../includes/config.php';
+$id = (int)($_GET['id'] ?? 0);
+if ($id <= 0) header('Location:index.php');
+$d = $mysqli->prepare("DELETE FROM teachers WHERE teacher_id=?");
+$d->bind_param('i', $id);
+$d->execute();
+header('Location:index.php');
+exit;
