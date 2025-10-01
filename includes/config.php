b14c117d0d465b27ea4e93e41e89baee06ef77a7
ImranBouroussi <155960049+ImranBouroussi@users.noreply.github.com>
2025-10-01 12:53:36 +0300

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+$DB_HOST = '127.0.0.1';
+$DB_USER = 'root';
+$DB_PASS = '';
+$DB_NAME = 'kurssihallinta';
+
+$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+if ($mysqli->connect_errno) {
+    die("MySQL connection failed: " . $mysqli->connect_error);
+}
+$mysqli->set_charset('utf8mb4');
+?>
